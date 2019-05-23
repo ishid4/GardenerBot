bd54e0c304deea1c870ba742ee924f3d9a7614c9
Erdemio <erdem_arslan_694@hotmail.com>
2019-05-23 16:49:29 +0300

    try

--- index.js
+++ index.js
@@ -41,15 +41,15 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/web'));
 
-app.get('/index.html', function(req, res) {});
+app.get('/', function(req, res) {});
 
 app.listen(app.get('port'), function() {
   console.log('Mounted ' + app.get('port'));
 });
 
-app.post ('/index.html', function(req, res) {
+app.post ('/', function(req, res) {
   if (req.method === 'POST') {
     let body = '';
     req.on('data', chunk => {
